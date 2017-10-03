0b22620f6ef781c99b19c4d08bcedd7f81858593
David <drgilmore@protonmail.com>
2017-10-03 11:24:07 -0600

    First Implementation

--- src/advprogjava/AdvProgJava.java
+++ src/advprogjava/AdvProgJava.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The open Kattis/ git homework.
+ * Problem URL: https://open.kattis.com/problems/cold
+ * David Gilmore
+ * 3 Oct 2017
  */
 package advprogjava;
 
@@ -15,8 +16,42 @@ public class AdvProgJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Do stuff and things
+        AdvProgJava program = new AdvProgJava();
+        program.run(args);
     }
     
+    public void run(String[] args)
+    {
+        int coldDays = daysBelow(args);
+        System.out.println("Days Below Zero: " + coldDays);
+    }
+    
+    public int daysBelow(String[] args)
+    {
+        int count = 0;
+        
+        for (int i = 0; i < args.length; i++)
+        {
+            int temp = 0;
+            
+            try
+            {
+                temp = Integer.parseInt(args[i]);
+            }
+            catch (Exception ex)
+            {
+                System.out.println("Unable to convert to integer.");
+            }
+            
+            if (temp < 0)
+            {
+                count++;
+            }    
+                 
+        }
+        
+        
+        
+      return count;  
+    }
 }
