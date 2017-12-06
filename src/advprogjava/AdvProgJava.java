e873254a2c0ee8a5b453541e5b8b72e629bcd26b
David <drgilmore@protonmail.com>
2017-12-06 06:04:25 -0700

    Parse Works

--- src/advprogjava/AdvProgJava.java
+++ src/advprogjava/AdvProgJava.java
@@ -5,6 +5,8 @@
  */
 package advprogjava;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -52,25 +54,173 @@ public class AdvProgJava {
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-            statement.executeUpdate("drop table if exists points");
-            statement.executeUpdate("create table points (XVal integer primary key, YVal integer)");
+            statement.executeUpdate("drop table if exists backlog");
+            statement.executeUpdate("create table backlog (Title string primary key, Genre string, Length string)");
         } catch (SQLException ex) {
             Logger.getLogger(AdvProgJava.class.getName()).log(Level.SEVERE, null, ex);
         }
         
 }
     
-    long insertPoint(int xval, int yval) {
+    int parse(String input)
+    {
+        String[] tokens  = input.toLowerCase().split(",");
+        
+        if (tokens[0].equals("add"))
+        {
+            if (tokens.length != 4)
+            {
+                System.out.println("Incorrect number of arguments for Add.");
+                return 1;
+            }
+            
+            //Sanitize
+            if (sanitize(tokens[2], tokens[3]))
+            {
+                
+            }
+            
+            //try
+            //result = insertgame(tokens[1], tokens[2], tokens[3]
+            //catch exception
+            //Sysout "Incorrect # of arguments for add
+            //
+            //if result = 1
+            // Sysout "Game successfully added."
+            
+            System.out.println("Add found");
+            
+            return 1;
+        }
+        else if (tokens[0].equals("remove"))
+        {
+            if (tokens.length != 2)
+            {
+                System.out.println("Incorrect number of arguments for Remove.");
+                return 1;
+            }
+            //try
+            //removegame(tokens[1])
+            //catch exception
+            //Sysout "Incorrect # of arguments for add
+            //
+            //if result = 1
+            // Sysout "Game successfully removed."
+            
+            System.out.println("Remove found");
+            
+            return 1;
+        }
+        else if (tokens[0].equals("list"))
+        {
+            //try
+            //resultSet rs = listGames
+            //while rs.next
+            //sysout rs.getString
+            
+            System.out.println("list found");
+            
+            return 1;
+        }
+        else if (tokens[0].equals("find"))
+        {
+            if (tokens.length != 3)
+            {
+                System.out.println("Incorrect number of arguments for Find.");
+                return 1;
+            }
+            //Sanitize
+            //rs = findGames(tokens[1], tokens[2])
+            //while rs.next
+            //sysout rs.getString
+            
+            System.out.println("find found");
+            
+            return 1;
+        }
+        else if (tokens[0].equals("search"))
+        {
+            if (tokens.length != 2)
+            {
+                System.out.println("Incorrect number of arguments for Search.");
+                return 1;
+            }
+            //try
+            //if searchgames(tokens[1])
+            //sysout "Game is in the database
+            //else
+            //sysout "game is not in the database"
+            
+            System.out.println("search found");
+            
+            return 1;
+        }
+        else if (tokens[0].equals("help"))
+        {
+            System.out.println("Valid Commands:\n");
+            System.out.println("Add,Title,Genre,Length to add a game.");
+            System.out.println("Remove,Title to remove a game.");
+            System.out.println("List to list all games.");
+            System.out.println("Find,Genre,Length to find games with the specified genre and length.");
+            System.out.println("Search,Title to see if a game is in the database.\n");
+            System.out.println("Valid Genres:\n");
+            System.out.println("Shooter, Multiplayer, RPG, Action, Fighting, OpenWorld, Other\n");
+            System.out.println("Valid Lengths:\n");
+            System.out.println("Multiplayer, Short, Medium, Long");
+            
+            return 1;
+        }
+        else if (tokens[0].equals("exit"))
+        {
+            return 0;
+        }
+        else
+        {
+            System.out.println("Command not found.");
+            
+            return 1;
+        }
+    }
+    
+    boolean sanitize(String inGenre, String inLength)
+    {
+        String[] genres = new String[]{"shooter", "multiplayer", "rpg", "action", "fighting", "openworld", "other"};
+        String[] lengths = new String[]{"multiplayer", "short", "medium", "long"};
+        
+        boolean flagGenre = false;
+        boolean flagLength = false;
+        
+        for (int i = 0; i < genres.length; i++)
+        {
+            if (inGenre.toLowerCase().equals(genres[i]))
+            {
+                flagGenre = true;
+            }
+        }
+        
+        for (int i = 0; i < lengths.length; i++)
+        {
+            if (inLength.toLowerCase().equals(lengths[i]))
+            {
+                flagLength = true;
+            }
+        }
+        
+        return flagGenre && flagLength;
+    }
+    
+    long insertGame(String inTitle, String inGenre, String inLength) {
         
         int nRows = 0;
         try {
             Connection connection = getConnection();
-            String sql = "insert or replace into points (XVal, YVal) values (?, ?)";
+            String sql = "insert or replace into backlog (Title, Genre, Length) values (?, ?, ?)";
 
             PreparedStatement ps
                     = connection.prepareStatement(sql);
-            ps.setInt(1, xval);
-            ps.setInt(2, yval);
+            ps.setString(1, inTitle);
+            ps.setString(2, inGenre);
+            ps.setString(3, inLength);
             nRows = ps.executeUpdate();
         } catch (SQLException ex) {
             Logger.getLogger(AdvProgJava.class.getName()).log(Level.SEVERE, null, ex);
@@ -79,34 +229,114 @@ public class AdvProgJava {
         return nRows;
 }
     
-    String getYVal(int xval) {
+    ResultSet findGames(String inGenre, String inLength)
+    {
         try {
             Connection connection = getConnection();
-            String sql = "select yval from points where xval=?";
+            String sql = "select Title from  backlog where Genre is ? and Length is ?";
+
             PreparedStatement ps
-                    = connection.prepareStatement(sql, Statement.NO_GENERATED_KEYS);
-            ps.setLong(1, xval);
-            ResultSet rs = ps.executeQuery();
-            if (rs != null && rs.next()) {
-                return rs.getString("YVal");
-            }
+                    = connection.prepareStatement(sql);
+            ps.setString(1, inGenre);
+            ps.setString(2, inLength);
+            ResultSet rs = ps.getResultSet();
+            return rs;
+//            int counter = 1;
+//            while (rs.next())
+//            {
+//                System.out.println(rs.getString(counter));
+//                counter++;
+//            }
         } catch (SQLException ex) {
             Logger.getLogger(AdvProgJava.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         return null;
-}
+    }
     
+    ResultSet listGames()
+    {
+         try {
+            Connection connection = getConnection();
+            String sql = "select Title from  backlog";
+
+            PreparedStatement ps
+                    = connection.prepareStatement(sql);
+            ResultSet rs = ps.getResultSet();
+            return rs;
+        } catch (SQLException ex) {
+            Logger.getLogger(AdvProgJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+    
+    int removeGame(String inTitle)
+    {
+     int nRows = 0;
+        try {
+            Connection connection = getConnection();
+            String sql = "delete from backlog where Title is ?";
+
+            PreparedStatement ps
+                    = connection.prepareStatement(sql);
+            ps.setString(1, inTitle);
+            nRows = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AdvProgJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return nRows;   
+    }
+    
+    boolean searchGame(String inTitle)
+    {
+        try {
+            Connection connection = getConnection();
+            String sql = "select Title from  backlog where Title is ?";
+
+            PreparedStatement ps
+                    = connection.prepareStatement(sql);
+            ps.setString(1, inTitle);
+            ResultSet rs = ps.getResultSet();
+            return inTitle.equals(rs.getString(1));                 
+        } catch (SQLException ex) {
+            Logger.getLogger(AdvProgJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return false;
+    }
     
     public void run()
     {
-        reset();
-        System.out.println(insertPoint(3, 7));
-        System.out.println(insertPoint(4, 8));
-        System.out.println(insertPoint(5, 9));
+        getConnection();
         
-        System.out.println(getYVal(3));
-        System.out.println(getYVal(4));
-        System.out.println(getYVal(5));
+        BufferedReader br = null;
+        
+        System.out.println("Good to go.");
+        
+        
+        try
+        {
+
+            br = new BufferedReader(new InputStreamReader(System.in));
+            int state = 0;
+
+            while (true)
+                {
+                    //read input
+                    state = parse(br.readLine());
+                    
+                    if (state == 0)
+                        break;
+                }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Something broke somewhere.");
+        }
+        
     }
     
+
 }
